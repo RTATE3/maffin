1335686b05342b783d92175e53a65a98d81f4a24
a-s-t-a-t-i-u-m <vasetanec@gmail.com>
2023-09-08 05:20:23 +0600

    dfdfd

--- main.py
+++ main.py
@@ -1,42 +1,55 @@
-# from tkinter import *
-# from tkinter import ttk
-#
-#
-# root = Tk()
-# root.title('Maffin')
-# frm = ttk.Frame(root, padding=100)
-# frm.grid()
-# ttk.Label(frm, text="""Приложение для управления личными финансами""").grid(column=0, row=0)
-# ttk.Button(frm, text="Закрыть", command=root.destroy).grid(column=0, row=5)
-# root.mainloop()
+
+
+
 
 import sqlite3
 import datetime
-transaction = input('Доходы или расходы? 1/0')
-current_date = datetime.datetime.now()
+from tkinter import *
+from tkinter import ttk
 
 
-class Expenses:
-
-    def __init__(self, categoria, summ):
-        self.categoria = categoria
-        self.summ = summ
+def expenses_func():
+    btn.config(state='disabled')
+    label.config(text='Введите сумму')
 
 
-with sqlite3.connect('maffin.db') as connection:
-    cursor = connection.cursor()
-    if int(transaction):
-        revenues = input('Сколько?')
-        cursor.execute('INSERT INTO FinTransaction (date, summ, categoria) VALUES (?, ?, ?)',
-                       (str(current_date), int(revenues), 'Доход'))
+def revenues_func():
+    btn1.config(state='disabled')
 
-    else:
-        expenses = Expenses(categoria=input('Введите категорию вашего расхода: '),
-                            summ=input('Введите сумму вашего расхода: '))
-        print(expenses.categoria)
-        print(expenses.summ)
-        # cursor.execute('INSERT INTO
 
+root = Tk()
+root.title('Maffin - приложение для управления личными финансами.')
+root.geometry("500x600")
+
+btn = ttk.Button(text="Добавить трату", command=expenses_func)
+btn1 = ttk.Button(text="Добавить доход", command=revenues_func)
+btn.pack(ipady=20)
+btn1.pack(ipady=18)
+
+label = ttk.Label(text="")
+label.pack(anchor=CENTER, expand=True)
+ttk.Entry().pack(anchor=N, padx=8, pady=8)
+
+
+
+
+root.mainloop()
+
+# transaction = int(input('Доходы или расходы? 1/0'))
+# current_date = str(datetime.datetime.now())
+#
+# with sqlite3.connect('maffin.db') as connection:
+#     cursor = connection.cursor()
+#     if transaction:
+#         revenues = int(input('Сколько?'))
+#         cursor.execute('INSERT INTO FinTransaction (date, summ, categoria) VALUES (?, ?, ?)',
+#                        (current_date, revenues, 'Доход'))
+#
+#     else:
+#         categoria = input('Введите категорию вашего расхода: ')
+#         summ = int(input('Введите сумму вашего расхода: '))
+#         cursor.execute('INSERT INTO FinTransaction (date, summ, categoria) VALUES (?, ?, ?)',
+#                        (current_date, summ, categoria))
 
 
 
